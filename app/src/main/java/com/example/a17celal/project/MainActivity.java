007872cdfd749d851863f3c11393ff513b315cb4
a17celal <celinefridalrud@hotmail.com>
2018-05-16 14:07:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a17celal/project/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.a17celal.project;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] gumNames = {"White Sweet Mint","Superberries","Fusion Strawberry lime", "Strength Spearmint", "Frost Peppermint", "Sweet menthol"};
+
+    private List<Tuggummi> namnen = new ArrayList<Tuggummi>();
+    private  ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        List<String> listData = new ArrayList<String>(Arrays.asList(gumNames));
+
+        adapter = new ArrayAdapter(getApplicationContext(),R.layout.list_item_textview,
+                R.id.my_item_textview, namnen);
+
+        ListView myListView = (ListView)findViewById(R.id.my_listview);
+
+        myListView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/a17celal/project/Tuggummi.java
@@ -0,0 +1,24 @@
+package com.example.a17celal.project;
+
+/**
+ * Created by a17celal on 2018-05-16.
+ */
+
+public class Tuggummi {
+    private String ID;
+    private String name;
+    private String company;
+    private String location;
+    private String category;
+    private int pris;
+
+    Tuggummi(String i, String n, String c, String l, String k, int p) {
+        ID=i;
+        name=n;
+        company=c;
+        location=l;
+        category=k;
+        pris=p;
+    }
+}
+
