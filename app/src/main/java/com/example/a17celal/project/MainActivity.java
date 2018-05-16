ebd2e45a2cd124859a5234409c124ccda5a6ae88
a17celal <celinefridalrud@hotmail.com>
2018-05-16 14:49:21 +0200

    list provat så att listan fungerar med en string (ej JSON) börjat med fetch+onpost..

--- app/src/main/java/com/example/a17celal/project/MainActivity.java
+++ app/src/main/java/com/example/a17celal/project/MainActivity.java
@@ -1,23 +1,34 @@
 package com.example.a17celal.project;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private String[] gumNames = {"White Sweet Mint","Superberries","Fusion Strawberry lime", "Strength Spearmint", "Frost Peppermint", "Sweet menthol"};
-
     private List<Tuggummi> namnen = new ArrayList<Tuggummi>();
     private  ArrayAdapter adapter;
 
@@ -27,20 +38,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        new Fetchdata().execute();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         List<String> listData = new ArrayList<String>(Arrays.asList(gumNames));
 
         adapter = new ArrayAdapter(getApplicationContext(),R.layout.list_item_textview,
-                R.id.my_item_textview, namnen);
+                R.id.my_item_textview, listData);
 
         ListView myListView = (ListView)findViewById(R.id.my_listview);
 
@@ -69,4 +73,77 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class Fetchdata extends AsyncTask<Void, Void, String>{
+
+        @Override
+        protected String doInBackground(Void... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String jsonStr = null;
+
+            try {
+                URL url = new URL("http://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=a17celal");
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                jsonStr = buffer.toString();
+                return jsonStr;
+            }
+                catch (IOException e) {
+                    Log.e("PlaceholderFragment", "Error ", e);
+                    return null;
+                }
+                finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    }
+                    catch (final IOException e) {
+                        Log.e("Network error", "Error closing stream", e);
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String o) {
+            super.onPostExecute(o);
+
+            Log.d("Hej", o);
+
+            try {
+                JSONArray allt=new JSONArray(o);
+                adapter.clear();
+                for (int r=0;r<allt.length();r++) {
+                    JSONObject obj=allt.getJSONObject(r);
+                    String name=obj.getString("name");
+                }
+            }
+            catch (JSONException e) {
+                Log.e("Hej", "E:"+e.getMessage());
+            }
+        }
+    }
+
 }
